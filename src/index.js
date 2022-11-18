9cc17e7c0f0c8f2dd6c9b31b33f6efefaab50106
NaNa923 <k5279kr@gmail.com>
2022-11-18 11:39:36 +0900

    git test3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/Reactbook15">
+  <BrowserRouter rbasename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
